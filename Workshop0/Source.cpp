3cc21b9be48bdb52a1b3dc7b0561dff6d9039d40
assaduzzaman3626 <assaduzzaman3626@gmail.com>
2025-09-17 19:26:42 -0400

    Updated source file

---
+++ Workshop0/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "#### Workshop Zero ####" << std::endl;
+    std::cout << "Subject and Section: " << std::endl;
+    std::cout << "OOP244-NRA" << std::endl;
+    std::cout << "Name:" << std::endl;
+    std::cout << "Md Assaduzzaman" << std::endl;
+    std::cout << "Student Number:" << std::endl;
+    std::cout << "122963242" << std::endl;
+    std::cout << "Email:" << std::endl;
+    std::cout << "massaduzzaman2@myseneca.ca" << std::endl;
+    return 0;
+}
